b4bf5c9515112e791a54d55068173c17821f77db
Alexander Rowland <rowlanda@protonmail.com>
2023-08-10 02:16:10 -0400

    Move calls to expense objects into Expense class, refactor to be methods

--- expense_report/expense_report.rb
+++ expense_report/expense_report.rb
@@ -6,6 +6,26 @@ class Expense
     @type = type
     @amount = amount
   end
+
+  def is_meal?
+    type == :dinner || type == :breakfast
+  end
+
+  def get_name
+    expenseName = ""
+    case type
+    when :breakfast
+        expenseName = "Breakfast"
+    when :dinner
+        expenseName = "Dinner"
+    when :car_rental
+        expenseName = "Car Rental"
+    end
+  end
+
+  def is_over_limit?
+    type == :dinner && amount > 5000 || type == :breakfast && amount > 1000
+  end
 end
 
 class ExpenseReport
@@ -15,20 +35,12 @@ class ExpenseReport
     # puts "Expenses: #{Time.now}"
     puts "Expenses: #{time}"
     for expense in expenses
-      if expense.type == :dinner || expense.type == :breakfast
+      if expense.is_meal?
         mealExpenses += expense.amount
       end
-      expenseName = ""
-      case expense.type
-      when :breakfast
-          expenseName = "Breakfast"
-      when :dinner
-          expenseName = "Dinner"
-      when :car_rental
-          expenseName = "Car Rental"
-      end
-      mealOverExpensesMarker = expense.type == :dinner && expense.amount > 5000 || expense.type == :breakfast && expense.amount > 1000 ? "X" : " "
-      puts "#{expenseName}\t#{expense.amount}\t#{mealOverExpensesMarker}"
+      expense_name = expense.get_name
+      mealOverExpensesMarker = expense.is_over_limit? ? "X" : " "
+      puts "#{expense_name}\t#{expense.amount}\t#{mealOverExpensesMarker}"
       total += expense.amount
     end
     puts "Meal Expenses: #{mealExpenses}" + "\n"
